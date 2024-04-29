6848bcb0d3228cb3dead5fe1ed0c10b93a5ae926
danielkurtsawyer <19danielsawyer@gmail.com>
2024-04-29 14:49:18 -0400

    Implement insert method

--- Tree.js
+++ Tree.js
@@ -24,6 +24,33 @@ class Tree {
 
     return root;
   }
+
+  insert(value){
+    this._root = this.insertRec(this._root, value);
+  }
+
+  insertRec(root, value){
+    // if the root is null, set the root to the new value 
+    if(root === null){
+      root = new Node(value);
+      return root;
+    }
+
+    // if not, recurse down the tree
+    if(value < root.data){
+      console.log('left');
+      root.leftChild = this.insertRec(root.leftChild, value);
+    } else if(value > root.data){
+      console.log('right');
+      root.rightChild = this.insertRec(root.rightChild, value);
+    }
+
+    return root;
+  }
+
+  deleteItem(value){
+
+  }
 }
 
 const prettyPrint = (node, prefix = "", isLeft = true) => {
@@ -40,4 +67,18 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
 };
 
 const tree1 = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
+// test buildTree
 prettyPrint(tree1.root);
+
+// test insert(value)
+tree1.insert(70);
+tree1.insert(6);
+prettyPrint(tree1.root);
+
+// test deletion at different depth levels
+// leaf 
+tree1.deleteItem(6);
+// middle of tree
+tree1.deleteItem(5);
+// root
+tree1.deleteItem(8);
